f883ca9563b9d3dd86db320318b64f2874ed2da0
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-13 15:25:40 +0300

    feat: update to establish mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}.`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+    process.exit(1);
+  });
