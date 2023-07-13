1220a39f8748ab1b42c00363b98d6ad416356987
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-13 14:53:10 +0300

    feat: add news routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+const newsRoutes = require("./routes/news");
+
 app.use(express.json());
+
+app.use("/", newsRoutes);
 
 module.exports = app;
