6371f2ea796713da3e5f3b3f4a54e8ac4c6ef519
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-13 16:25:00 +0300

    feat: add getNews function

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const newsController = require("../controllers/news");
 
-router.get("/");
+router.get("/", newsController.getNews);
 router.get("/:id");
 router.post("/");
 router.put("/:id");
