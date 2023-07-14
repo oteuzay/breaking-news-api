0dab9dcc71b3370c435c371b367c478be9433cbc
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-14 10:39:21 +0300

    feat: add getNewsById function

--- routes/news.js
+++ routes/news.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const newsController = require("../controllers/news");
 
 router.get("/", newsController.getNews);
-router.get("/:id");
+router.get("/:id", newsController.getNewsById);
 router.post("/", newsController.createNews);
 router.put("/:id");
 router.delete("/:id");
