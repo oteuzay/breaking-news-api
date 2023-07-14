0d68ac288062caa4e9b057bbe8df21f9251c1338
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-14 10:40:54 +0300

    feat: update to retrieve a single news

--- controllers/news.js
+++ controllers/news.js
@@ -10,7 +10,19 @@ exports.getNews = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.getNewsById = (req, res, next) => {};
+exports.getNewsById = (req, res, next) => {
+  const id = req.params.id;
+
+  News.findById(id)
+    .then((news) => {
+      res.status(200).json({
+        news: news,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.createNews = (req, res, next) => {
   const title = req.body.title;
