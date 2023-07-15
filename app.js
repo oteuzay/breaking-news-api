db02d4971c10350add16b265ba699048e3fff3cf
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-15 11:03:30 +0300

    feat: add error handling middleware

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ app.use(express.json());
 
 app.use("/", newsRoutes);
 
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+
+  res.status(status).json({
+    message: error.message,
+    data: error.data,
+  });
+});
+
 module.exports = app;
