217177b5851d8994cd606edf88db848b8ebfb7b2
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-16 15:33:13 +0300

    refactor: :art: update code format from promise to async/await

--- controllers/news.js
+++ controllers/news.js
@@ -1,44 +1,51 @@
 const News = require("../models/news");
 
-exports.getNews = (req, res, next) => {
-  News.find()
-    .then((news) => {
-      res.status(200).json({
-        news: news,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+exports.getNews = async (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 10;
+
+  try {
+    const countOfNews = await News.find().countDocuments();
+    const news = await News.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+
+    res.status(200).json({
+      news: news,
+      countOfNews: countOfNews,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.getNewsById = (req, res, next) => {
+exports.getNewsById = async (req, res, next) => {
   const id = req.params.id;
 
-  News.findById(id)
-    .then((news) => {
-      if (!news) {
-        const error = new Error("Could not find news.");
-        error.statusCode = 404;
-        throw error;
-      }
+  try {
+    const news = await News.findById(id);
 
-      res.status(200).json({
-        news: news,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    if (!news) {
+      const error = new Error("Could not find news.");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      news: news,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.createNews = (req, res, next) => {
+exports.createNews = async (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
 
@@ -47,77 +54,74 @@ exports.createNews = (req, res, next) => {
     content: content,
   });
 
-  news
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "News created.",
-        news: news,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    await news.save();
+
+    res.status(201).json({
+      message: "News created.",
+      news: news,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.updateNews = (req, res, next) => {
+exports.updateNews = async (req, res, next) => {
   const id = req.params.id;
 
   const title = req.body.title;
   const content = req.body.content;
 
-  News.findById(id)
-    .then((news) => {
-      if (!news) {
-        const error = new Error("Could not find news.");
-        error.statusCode = 404;
-        throw error;
-      }
+  try {
+    const news = await News.findById(id);
 
-      news.title = title;
-      news.content = content;
+    if (!news) {
+      const error = new Error("Could not find news.");
+      error.statusCode = 404;
+      throw error;
+    }
 
-      return news.save();
-    })
-    .then((result) => {
-      res.status(200).json({
-        message: "News updated",
-        news: result,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    news.title = title;
+    news.content = content;
+
+    const result = await news.save();
+
+    res.status(200).json({
+      message: "News updated",
+      news: result,
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
-exports.deleteNews = (req, res, next) => {
+exports.deleteNews = async (req, res, next) => {
   const id = req.params.id;
 
-  News.findById(id)
-    .then((news) => {
-      if (!news) {
-        const error = new Error("Could not find news.");
-        error.statusCode = 404;
-        throw error;
-      }
+  try {
+    const news = await News.findById(id);
 
-      return News.findByIdAndRemove(id);
-    })
-    .then(() => {
-      res.status(200).json({
-        message: "Deleted news.",
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    if (!news) {
+      const error = new Error("Could not find news.");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    await News.findByIdAndRemove(id);
+
+    res.status(200).json({
+      message: "Deleted news.",
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
