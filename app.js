d975ab67161cb7bc26687515f14b40039872966a
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-16 20:15:20 +0300

    feat: :sparkles: add compression and auth router

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const compression = require("compression");
 
 const app = express();
 
 const newsRoutes = require("./routes/news");
+const authRoutes = require("./routes/auth");
 
 app.use(express.json());
+app.use(compression());
 
 app.use("/", newsRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
