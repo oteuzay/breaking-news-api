bb7aeeedeb116e4a68380828abff1efb288de1bc
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-16 20:16:33 +0300

    feat: :sparkles: add auth middleware and implement to router

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,19 @@
 const router = require("express").Router();
 
 const newsController = require("../controllers/news");
+const isAuth = require("../middleware/is-auth");
 
+// GET
 router.get("/", newsController.getNews);
 router.get("/:id", newsController.getNewsById);
-router.post("/", newsController.createNews);
-router.put("/:id", newsController.updateNews);
-router.delete("/:id", newsController.deleteNews);
+
+// POST
+router.post("/", isAuth, newsController.createNews);
+
+// PUT
+router.put("/:id", isAuth, newsController.updateNews);
+
+// DELETE
+router.delete("/:id", isAuth, newsController.deleteNews);
 
 module.exports = router;
