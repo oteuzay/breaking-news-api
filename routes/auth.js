cff8a580b663653a72a70006c849c0f48d03f1b2
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-17 18:01:40 +0300

    feat: :sparkles: add validators and implement to routers

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const authController = require("../controllers/auth");
 
+const authValidator = require("../validators/auth");
+
 const router = express.Router();
 
 // GET
 router.post("/login", authController.login);
 
 // POST
-router.post("/signup", authController.signup);
+router.post("/signup", authValidator.signup, authController.signup);
 
 module.exports = router;
--- routes/news.js
+++ routes/news.js
@@ -3,15 +3,17 @@ const router = require("express").Router();
 const newsController = require("../controllers/news");
 const isAuth = require("../middleware/is-auth");
 
+const newsValidator = require("../validators/news");
+
 // GET
 router.get("/", newsController.getNews);
 router.get("/:id", newsController.getNewsById);
 
 // POST
-router.post("/", isAuth, newsController.createNews);
+router.post("/", isAuth, newsValidator.createNews, newsController.createNews);
 
 // PUT
-router.put("/:id", isAuth, newsController.updateNews);
+router.put("/:id", isAuth, newsValidator.updateNews, newsController.updateNews);
 
 // DELETE
 router.delete("/:id", isAuth, newsController.deleteNews);
