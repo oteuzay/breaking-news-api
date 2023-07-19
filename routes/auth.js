1a9c7466b7e8761adfcfb5f9f2dae782b791a4cc
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-19 19:28:55 +0300

    feat: update auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,12 @@
 const express = require("express");
 
 const authController = require("../controllers/auth");
-
 const authValidator = require("../validators/auth");
 
 const router = express.Router();
 
-// GET
-router.post("/login", authController.login);
-
 // POST
+router.post("/signin", authValidator.signin, authController.signin);
 router.post("/signup", authValidator.signup, authController.signup);
 
 module.exports = router;
