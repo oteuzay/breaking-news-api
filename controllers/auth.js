4367885926a6c25f33e6bd7150440aeafffdb74a
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-19 19:53:44 +0300

    feat: update auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,50 +1,23 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/user");
+const Editor = require("../models/editor");
 
 require("dotenv").config();
 
-exports.signup = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
+exports.signin = async (req, res, next) => {
+  const { email, password } = req.body;
 
   try {
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const editor = await Editor.findOne({ email: email });
 
-    const user = new User({
-      email: email,
-      password: hashedPassword,
-    });
-
-    const result = await user.save();
-
-    res.status(201).json({
-      message: "User created.",
-      userId: result._id,
-    });
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }
-};
-
-exports.login = async (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  try {
-    const user = await User.findOne({ email: email });
-
-    if (!user) {
+    if (!editor) {
       const error = new Error("Email could not be found.");
       error.statusCode = 401;
       throw error;
     }
 
-    const isEqual = await bcrypt.compare(password, user.password);
+    const isEqual = await bcrypt.compare(password, editor.password);
 
     if (!isEqual) {
       const error = new Error("Wrong password!");
@@ -54,21 +27,60 @@ exports.login = async (req, res, next) => {
 
     const token = jwt.sign(
       {
-        email: user.email,
-        userId: user._id.toString(),
+        email: editor.email,
+        userId: editor._id.toString(),
       },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
     res.status(200).json({
+      email: editor.email,
       token: token,
-      userId: user._id.toString(),
     });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
+
+    next(err);
+  }
+};
+
+exports.signup = async (req, res, next) => {
+  const { name, surname, email, password } = req.body;
+
+  try {
+    const findEditor = await Editor.findOne({ email: email });
+
+    if (findEditor) {
+      const error = new Error("E-Mail address already exists!");
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    await new Editor({
+      name: name,
+      surname: surname,
+      email: email,
+      password: hashedPassword,
+    }).save();
+
+    res.status(201).json({
+      message: "Editor successfully created.",
+      editor: {
+        name: name,
+        surname: surname,
+        email: email,
+      },
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+
     next(err);
   }
 };
