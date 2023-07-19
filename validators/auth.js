d8ce53b69e99896124c3a994fe19c1268c9ce7eb
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-19 19:19:28 +0300

    feat: update auth validators

--- validators/auth.js
+++ validators/auth.js
@@ -1,21 +1,34 @@
 const { body } = require("express-validator");
 
-const User = require("../models/user");
-
 const validationCheck = require("../middleware/validation-check");
 
 exports.signup = [
+  body("name").trim().notEmpty().withMessage("Name field should not be empty."),
+  body("surname")
+    .trim()
+    .notEmpty()
+    .withMessage("Surname field should not be empty."),
   body("email")
+    .trim()
     .isEmail()
     .withMessage("Please enter a valid email.")
-    .custom((value, { req }) => {
-      return User.findOne({ email: value }).then((userDoc) => {
-        if (userDoc) {
-          return Promise.reject("E-Mail address already exists!");
-        }
-      });
-    })
     .normalizeEmail(),
-  body("password").trim().isLength({ min: 5 }),
+  body("password")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long."),
+  validationCheck,
+];
+
+exports.signin = [
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please enter a valid email.")
+    .normalizeEmail(),
+  body("password")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long."),
   validationCheck,
 ];
