1064686dfa3c6e12d302ab180cffd8a8a265399e
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-19 15:26:19 +0300

    feat: add name and surname fields to editor model schema

--- models/editor.js
+++ models/editor.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 
 const editorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
