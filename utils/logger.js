de99bfb7b88ba31df698f98c18d7c180d48bc285
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-21 21:11:24 +0300

    feat: update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,14 +1,31 @@
 const winston = require("winston");
 
-const logConfiguration = {
+const logger = winston.createLogger({
+  levels: winston.config.npm.levels,
   transports: [
     new winston.transports.File({
       level: "error",
-      filename: "logs/errors.log",
+      filename: "./logs/errors.log",
+      handleExceptions: true,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+        winston.format.prettyPrint()
+      ),
+    }),
+    new winston.transports.Console({
+      level: "debug",
+      handleExceptions: true,
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ level, message, timestamp }) => {
+          const ts = timestamp.slice(0, 19).replace("T", " ");
+          return `${ts} [${level.toUpperCase()}]: ${message}`;
+        })
+      ),
     }),
   ],
-};
-
-const logger = winston.createLogger(logConfiguration);
+  exitOnError: false,
+});
 
 module.exports = logger;
