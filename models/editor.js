02efc5534558f0fadb0dd85b40951e3ed7f850d5
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-27 23:42:23 +0300

    feat: :sparkles: add role to editorSchema

--- models/editor.js
+++ models/editor.js
@@ -1,29 +1,39 @@
 const mongoose = require("mongoose");
 
-const editorSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  surname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  news: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "News",
+const editorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
       required: true,
     },
-  ],
-});
+    surname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "Editor",
+      required: true,
+    },
+    news: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "News",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Editor", editorSchema);
