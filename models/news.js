6c3196fd047f4e9a10632ff444248a24e468f3f5
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-28 21:30:27 +0300

    feat: add methods

--- models/news.js
+++ models/news.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const convertDateFormat = require("../helpers/convert-date-format");
+
 const newsSchema = new mongoose.Schema(
   {
     title: {
@@ -29,5 +31,43 @@ const newsSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+/* The `newsSchema.methods.toJSONForNews` function is a method that is added to the `newsSchema`
+object. This method is used to convert a news document from the MongoDB database into a JSON object
+with specific properties. */
+newsSchema.methods.toJSONForNews = function () {
+  return {
+    id: this._id,
+    title: this.title,
+    description: this.description,
+    content: this.content,
+    tags: this.tags,
+    createdAt: convertDateFormat(this.createdAt),
+    updatedAt: convertDateFormat(this.updatedAt),
+    editor: {
+      id: this.editorID._id,
+      name: this.editorID.name,
+      surname: this.editorID.surname,
+    },
+  };
+};
+
+/* The `newsSchema.methods.toJSONForPreviewOfNews` function is a method that is added to the
+`newsSchema` object. This method is used to convert a news document from the MongoDB database into a
+JSON object with specific properties, but with a simplified structure compared to the
+`toJSONForNews` method. */
+newsSchema.methods.toJSONForPreviewOfNews = function () {
+  return {
+    id: this._id,
+    title: this.title,
+    description: this.description,
+    createdAt: convertDateFormat(this.createdAt),
+    editor: {
+      id: this.editorID._id,
+      name: this.editorID.name,
+      surname: this.editorID.surname,
+    },
+  };
+};
 
 module.exports = mongoose.model("News", newsSchema);
