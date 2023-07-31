6bc0f34421d40df63aa7e3e59a5464b4fde3f740
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-31 17:34:39 +0300

    feat: implement swagger

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 
 const compression = require("compression");
 
-const errorHandler = require("./middleware/error-handler");
+const errors = require("./middleware/error-handler");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./docs/swagger");
 
 const app = express();
 
@@ -12,17 +15,26 @@ const newsRoutes = require("./routes/news");
 const authRoutes = require("./routes/auth");
 const editorRoutes = require("./routes/editor");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN,
+  })
+);
+
 app.use(helmet());
 
 app.use(express.json());
 app.use(compression());
+
+if (process.env.NODE_ENV == "Development") {
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+}
 
 app.use("/auth", authRoutes);
 
 app.use("/news", newsRoutes);
 app.use("/editors", editorRoutes);
 
-app.use(errorHandler);
+app.use(errors);
 
 module.exports = app;
