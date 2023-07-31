4242f2626ebf698c091d85641de7a51ec8d5f503
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-07-31 17:35:37 +0300

    feat: create a swaggerJSDoc

---
+++ docs/swagger.js
@@ -0,0 +1,21 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+/* The `package.json` file typically contains metadata about the project, including
+the project name, version, description, dependencies, and other information. In this case, it is
+being used to retrieve the name, version, and description of the project to populate the Swagger
+documentation. */
+const package = require("../package.json");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: package.name,
+      version: package.version,
+      description: package.description,
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+module.exports = swaggerJSDoc(options);
