5a0d1fa76f2bcc57eeaefe98ebfa12734175fe86
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-02 12:58:40 +0300

    chore: delete editor files

--- routes/editor.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const editorController = require("../controllers/editor");
-
-// GET
-router.get("/", editorController.getEditors);
-router.get("/:id", editorController.getEditor);
-
-module.exports = router;
