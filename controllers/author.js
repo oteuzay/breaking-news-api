08974b74610d65009849f95e933e838c6c5ee0e7
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-02 12:56:39 +0300

    refactor: update namings

---
+++ controllers/author.js
@@ -0,0 +1,35 @@
+const Author = require("../models/author");
+
+const CustomError = require("../utils/errors");
+
+exports.getAuthors = async (req, res, next) => {
+  try {
+    const authors = await Author.find();
+
+    if (!authors) {
+      throw new CustomError("Not Found", 404);
+    }
+
+    res.status(200).json({
+      authors: authors.map((author) => author.toJSONForSummaryOfAuthor()),
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAuthor = async (req, res, next) => {
+  try {
+    const author = await Author.findById(req.params.id).populate("news");
+
+    if (!author) {
+      throw new CustomError("Author could not be found.", 404);
+    }
+
+    res.status(200).json({
+      author: author,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
