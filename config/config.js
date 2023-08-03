5c2119fed34c30885558e0f8ef393b8c3dd263b1
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-04 01:41:02 +0300

    feat: add default values in configuration

--- config/config.js
+++ config/config.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 /* The code is exporting an object with several properties. Each property is assigned a value from the
 corresponding environment variable using `process.env`. */
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 8080,
   DB: process.env.DB,
   SECRET: process.env.SECRET,
-  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN,
-  NODE_ENV: process.env.NODE_ENV,
+  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN | "*",
+  NODE_ENV: process.env.NODE_ENV || "Development",
 };
