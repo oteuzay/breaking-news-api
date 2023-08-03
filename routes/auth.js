36337f6c03928ac23c5f9569a9441203cdd3f589
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-03 18:31:57 +0300

    feat: add document to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,8 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ */
 const express = require("express");
 
 const authController = require("../controllers/auth");
@@ -5,8 +10,76 @@ const authValidator = require("../validators/auth");
 
 const router = express.Router();
 
-// POST
+/**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: example@example.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: password123
+ *     responses:
+ *       200:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not Found
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post("/signin", authValidator.signin, authController.signin);
+
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: John
+ *               surname:
+ *                 type: string
+ *                 example: Doe
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: example@example.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: 12345
+ *     responses:
+ *       201:
+ *         description: Success
+ *       400:
+ *         description: Bad Request
+ *       422:
+ *         description: Unprocessable Entity
+ *       500:
+ *         description: Internal Server Error
+ */
 router.post("/signup", authValidator.signup, authController.signup);
 
 module.exports = router;
