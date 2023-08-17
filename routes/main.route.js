b54f29f80dbf994f71ecb0754ab2444745477a06
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-18 01:34:27 +0300

    feat: add api-docs path to main route

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 
+const config = require("../config/api.config");
+
 const newsRoute = require("./news.route");
 const authRoute = require("./auth.route");
 const authorRoute = require("./author.route");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("../config/swagger");
+
+if (config.NODE_ENV === "Development") {
+  router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+}
 
 router.use("/news", newsRoute);
 router.use("/auth", authRoute);
