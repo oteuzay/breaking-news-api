2a552401d51cb519a03c80d99af4571a7407996d
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-17 21:43:23 +0300

    feat: add new methods to author repository

--- repositories/author.repository.js
+++ repositories/author.repository.js
@@ -1,12 +1,20 @@
-const Author = require("../models/author");
+const Author = require("../models/author.model");
 
 class AuthorRepository {
+  async createAuthor(author) {
+    return await Author.create(author);
+  }
+
   async getAuthors() {
     return await Author.find();
   }
 
-  async getAuthor(authorID) {
-    return Author.findById(authorID).populate("news");
+  async getAuthorById(authorID) {
+    return await Author.findById(authorID).populate("news");
+  }
+
+  async getAuthorByEmail(email) {
+    return Author.findOne({ email: email });
   }
 
   async addNewsToAuthor(newsID, authorID) {
