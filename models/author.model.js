5ef7fca9c10252789eda9828b03f0dc6ae723d43
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-17 21:45:45 +0300

    feat: add pre save function to author model

--- models/author.model.js
+++ models/author.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const convertDateFormat = require("../helpers/convert-date-format");
+const bcrypt = require("bcryptjs/dist/bcrypt");
 
 const authorSchema = new mongoose.Schema(
   {
@@ -60,5 +61,18 @@ authorSchema.methods.toJSON = function () {
     createdAt: convertDateFormat(this.createdAt),
   };
 };
+
+authorSchema.pre("save", async function (next) {
+  try {
+    if (this.isNew) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = mongoose.model("Author", authorSchema);
