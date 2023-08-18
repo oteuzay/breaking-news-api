406a0d49aac4119eba617a7b1c09dd626c4f65ea
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-19 00:33:50 +0300

    feat: update api config file and clean up

--- config/api.config.js
+++ config/api.config.js
@@ -4,8 +4,6 @@ require("dotenv").config();
 corresponding environment variable using `process.env`. */
 module.exports = {
   PORT: process.env.PORT || 8080,
-  DB: process.env.DB,
-  SECRET: process.env.SECRET,
   ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN | "*",
   NODE_ENV: process.env.NODE_ENV || "Development",
   LOCALE: process.env.LOCALE,
