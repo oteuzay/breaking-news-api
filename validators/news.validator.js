10af21956d82095185b350682ae98cd9d044a2f3
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-18 22:42:34 +0300

    feat: update news validator and clean up

--- validators/news.validator.js
+++ validators/news.validator.js
@@ -1,8 +1,18 @@
 const { body, check, query } = require("express-validator");
 
-const validationCheck = require("../middleware/validation-check");
+const validationCheck = require("../middleware/validation-check.middleware");
 
-const validateCommonFields = [
+exports.getNews = [
+  check("page")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage(
+      "Page must be a positive integer and greater than or equal to 1."
+    ),
+  validationCheck,
+];
+
+exports.createNews = [
   body("title")
     .trim()
     .isLength({ min: 5 })
@@ -17,8 +27,8 @@ const validateCommonFields = [
     .withMessage("Description must be a string."),
   body("content")
     .trim()
-    .isLength({ min: 20 })
-    .withMessage("Content must be at least 20 characters long.")
+    .isLength({ min: 5 })
+    .withMessage("Content must be at least 5 characters long.")
     .isString()
     .withMessage("Content must be a string."),
   body("tags")
@@ -31,28 +41,54 @@ const validateCommonFields = [
       }
       return true;
     }),
-];
-
-const validateId = [
-  check("id")
-    .isMongoId()
-    .withMessage("Invalid news ID. The ID should be a valid MongoDB ObjectID."),
-];
-
-exports.getNews = [
-  check("page")
-    .optional()
-    .isInt({ min: 1 })
-    .withMessage(
-      "Page must be a positive integer and greater than or equal to 1."
-    ),
   validationCheck,
 ];
 
-exports.createNews = [...validateCommonFields, validationCheck];
+exports.getNewsById = [
+  check("id")
+    .isMongoId()
+    .withMessage("Invalid news ID. The ID should be a valid MongoDB ObjectID."),
+  validationCheck,
+];
 
-exports.updateNews = [...validateCommonFields, validationCheck];
+exports.updateNews = [
+  check("id")
+    .isMongoId()
+    .withMessage("Invalid news ID. The ID should be a valid MongoDB ObjectID."),
+  body("title")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Title must be at least 5 characters long.")
+    .isString()
+    .withMessage("Title must be a string."),
+  body("description")
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage("Description must be at least 10 characters long.")
+    .isString()
+    .withMessage("Description must be a string."),
+  body("content")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("Content must be at least 5 characters long.")
+    .isString()
+    .withMessage("Content must be a string."),
+  body("tags")
+    .optional()
+    .isArray()
+    .withMessage("Tags must be an array.")
+    .custom((tags) => {
+      if (!tags.every((tag) => typeof tag === "string")) {
+        throw new Error("All tags must be strings.");
+      }
+      return true;
+    }),
+  validationCheck,
+];
 
-exports.getNewsById = [...validateId, validationCheck];
-
-exports.deleteNews = [...validateId, validationCheck];
+exports.deleteNews = [
+  check("id")
+    .isMongoId()
+    .withMessage("Invalid news ID. The ID should be a valid MongoDB ObjectID."),
+  validationCheck,
+];
