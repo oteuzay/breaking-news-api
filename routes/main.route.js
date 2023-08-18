833226f36a6872b01bd96cdd8ac0609e2cd4961e
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-18 19:55:05 +0300

    feat: implement swagger route to main route

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,17 +1,12 @@
 const router = require("express").Router();
 
-const config = require("../config/api.config");
-
 const newsRoute = require("./news.route");
 const authRoute = require("./auth.route");
 const authorRoute = require("./author.route");
 
-const swaggerUI = require("swagger-ui-express");
-const { swaggerSpec } = require("../utils/swagger.util");
+const swaggerRoute = require("./swagger.route");
 
-if (config.NODE_ENV === "Development") {
-  router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-}
+router.use("/api-docs", swaggerRoute);
 
 router.use("/news", newsRoute);
 router.use("/auth", authRoute);
