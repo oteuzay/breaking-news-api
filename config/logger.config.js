24cd33300ecd7764c3eccfdd27014444e7c60221
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-08-18 14:56:04 +0300

    feat: create a logger config

---
+++ config/logger.config.js
@@ -0,0 +1,10 @@
+/* The code is exporting an object with two properties: `FILE` and `CONSOLE`. */
+module.exports = {
+  FILE: {
+    LEVEL: "error",
+    FILENAME: "./logs/errors.log",
+  },
+  CONSOLE: {
+    LEVEL: "debug",
+  },
+};
